7c6cb6cb4874acbcba2b9826c906bc00c301f31e
PreetiHatti <preeti.hatti@viooh.com>
2023-08-20 06:29:49 +0100

    First commit

---
+++ design_principles+-+OC+-+begin/design_principles - OC - begin/src/main/java/com/company/openclose/ISPSubscriber.java
@@ -0,0 +1,20 @@
+package com.company.openclose;
+
+import java.util.List;
+
+public class ISPSubscriber  extends  Subscriber{
+
+private long freeUsage;
+
+
+
+    @Override
+    public double calculateBill() {
+        List<InternetSessionHistory.InternetSession> sessions = InternetSessionHistory.getCurrentSessions(subscriberId);
+        long totalData = sessions.stream().mapToLong(InternetSessionHistory.InternetSession::getDataUsed).sum();
+        long chargeableData = totalData - freeUsage;
+        return chargeableData*baseRate/100;
+    }
+
+
+}
---
+++ design_principles+-+liskov+-+begin/design_principles - liskov - begin/src/main/java/com/company/liskov_1/Square.java
@@ -0,0 +1,13 @@
+package com.company.liskov_1;
+
+public class Square implements Shape {
+ private int side;
+
+    public Square(int side) {
+	this.side = side;
+    }
+
+    @Override public int computeArea() {
+	return side*side;
+    }
+}
